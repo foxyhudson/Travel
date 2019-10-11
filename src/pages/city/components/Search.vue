fe69509816360fa803bc059292d65d26e5912e6a
foxyhudson <foxyhudson@qq.com>
2019-10-11 22:19:03 +0800

    city-search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
-    <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <div>
+      <div class="search">
+        <input  v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+      </div>
+      <div v-show="keyword" class="serach-content" ref="serach">
+        <ul>
+          <li class="search-time border-bottom" v-for ="item of list" :key="item.id" >{{item.name}}</li>
+          <li v-show="hasNoData" class="search-time border-bottom">没有找到匹配数据</li>
+        </ul>
+      </div>
     </div>
 </template>
 
 <script>
+    import Bscroll from "better-scroll"
     export default {
-        name: "CitySearch"
+        name: "CitySearch",
+        props:{
+          cities:Object
+        },
+        data(){
+          return{
+            keyword:"",
+            list:[],
+            timer:null, //节流
+          }
+        },
+        computed:{
+          hasNoData(){
+            return !this.list.length
+          }
+        },
+        mounted(){
+          this.search = new Bscroll(this.$refs.serach)
+        },
+        watch:{
+          keyword(){
+            if(this.timer){
+              clearTimeout(this.timer)
+            }
+            if(!this.keyword){
+              this.list=[]
+              return
+            }
+            this.timer = setTimeout(()=>{
+              const result = []
+              for (let i in this.cities){
+                this.cities[i].forEach((value)=>{
+                  if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1){
+                    result.push(value)
+                  }
+                })
+              }
+              this.list =result
+            },100)
+          },
+        }
     }
 </script>
 
@@ -25,5 +74,19 @@
       border-radius: .06rem
       color: #666
       padding : 0 .1rem
-
+  .serach-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-time
+      height :.62rem
+      line-height:.62rem
+      padding-left:.2rem
+      color:#666
+      background: #fff
 </style>
