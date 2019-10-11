dce206df3fc238874795f32a3461a63f592a505d
foxyhudson <foxyhudson@qq.com>
2019-10-11 11:00:43 +0800

    modify

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <swiper :options="swiperOption" ref="mySwiper" >
       <!-- slides -->
-      <swiper-slide v-for="item of swiperList" key="item.id">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
 
@@ -12,36 +12,40 @@
 </template>
 
 <script>
-    export default {
-        name: "HomeSwiper",
-        data () {
-          return{
-            swiperOption:{
-              pagination:'.swiper-pagination',
-              loop: true
-            },
-            swiperList:[{
-              id:'0001',
-              imgUrl:'https://i0.hdslb.com/bfs/sycp/creative_img/201910/723f2b2ed088fa894e864b1bad13de32.jpg@1100w_484h_1c_100q.jpg'
-            },
-            {
-              id:'0002',
-              imgUrl:'https://img1.5173.com//e535e2aead824a4d8539b6edda7c3db1.jpg'
-            }]
-          }
-        }
+  export default {
+    name: "HomeSwiper",
+    data () {
+      return{
+        swiperOption:{
+          pagination:'.swiper-pagination',
+          loop: true
+        },
+        swiperList:[{
+          id:'0001',
+          imgUrl:'https://i0.hdslb.com/bfs/sycp/creative_img/201910/723f2b2ed088fa894e864b1bad13de32.jpg@1100w_484h_1c_100q.jpg'
+        },
+          {
+            id:'0002',
+            imgUrl:'https://sk.5173cdn.com/sk_4/data/201909/00/C1/SwKowF2RUGAAAAAAAAgpIw5PJ8088.png'
+          }]
+      }
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
     background: #fff
+
   .wrapper
-    width:100%
-    height:31.25vw
-    background:#ccc
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
     .swiper-img
-      width:100%
-      height :3.5rem
-      overflow hidden
+      width: 100%
+      height : 2.6rem
+
+
 </style>
