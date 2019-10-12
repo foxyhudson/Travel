0b77dd5658605d5e70d66436ad45e38956ed238d
foxyhudson <foxyhudson@qq.com>
2019-10-12 14:16:52 +0800

    detal-banner

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <swiper-slide class="juzhong" v-for="(item,index) of imgs" :key="imgs.index">
+<!--          <img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_r_800x800_9b21ceda.jpg" />-->
+          <img class="gallary-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "CommonGallary",
+        props:{
+          imgs:{ //组件需要从外面拿图片
+            type:Array,
+            default (){ //默认值
+              return [] //默认图片
+            }
+          }
+        },
+        methods:{
+          handleGallaryClick(){
+            this.$emit('close')
+          }
+        },
+        data(){
+          return{
+            swiperOptions:{
+              pagination:'.swiper-pagination',
+              paginationType: 'fraction', //页码
+              observeParents: true,   //监听到dom变化 自我刷新 不然页面进来展示可能由于宽度会出现异常
+              observer:true           //监听到dom变化 自我刷新 不然页面进来展示可能由于宽度会出现异常
+        }
+          }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container  //默认hidden所以要越界改
+      overflow: inherit
+
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+
+</style>
---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+      <div class="banner" @click="handleBennerClick">
+        <img clas="banner-img" src="//img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_600x330_3f524da7.jpg" alt="">
+        <div class="banner-info">
+          <div class="banner-title">奥林匹克塔</div>
+          <div class="banner-number">
+            <span class="iconfont icontupian banner-icon" ></span>
+            8
+          </div>
+        </div>
+      </div>
+      <common-gallary
+        @close = "handleGallaryClose"
+        :imgs="imgs" v-show="showGallary"></common-gallary>
+    </div>
+</template>
+
+<script>
+    import CommonGallary from 'common/gallary/Gallary'
+    export default {
+        name: "DetailBanner",
+        components:{
+          CommonGallary
+          },
+        data(){
+          return{
+            showGallary:false,
+            imgs:['http://img1.qunarzz.com/sight/p0/1804/5f/5f59c2661add425aa3.img.jpg_r_800x800_ff390240.jpg',
+                  'http://img1.qunarzz.com/sight/p0/1804/b6/b68ccb778c685283a3.img.jpg_r_800x800_da0c0853.jpg'
+            ]
+          }
+        },
+        methods:{
+          handleBennerClick(){
+            this.showGallary=true
+          },
+          handleGallaryClose(){ //关闭画廊
+            this.showGallary = false
+          }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .banner  //防抖动
+    overflow:hidden
+    height: 0
+    padding-bottom : 55%
+    position: relative
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left:0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.8)) //渐变
+      .banner-title
+        flex:1
+        font-size: .32rem
+        padding : 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        line-height: .32rem
+        height: .32rem
+        border-radius: .2rem
+        background: rgba(0,0,0,.8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
