cc6b52dcdc34883454213f5d2161e469f3b4cd88
foxyhudson <foxyhudson@qq.com>
2019-10-15 21:43:01 +0800

    fn

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -3,7 +3,7 @@
         <li class="item" v-for="item of letters"  :key="item"
             :ref="item"
           @click="handleLetterClick"
-          @touchstart="handleTouchStart"
+          @touchstart.prevent="handleTouchStart"
           @touchmove="handleTouchMove"
           @touchend="handleTouchEnd"
         >{{item}}</li>
